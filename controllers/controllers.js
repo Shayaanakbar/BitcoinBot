838cb0289cafe9a4a57cde154f3f06be289ff53d
ShayaanAkbar <shayaanakbar1106@gmail.com>
2021-05-20 20:42:23 -0400

    Bitcoin price is updating every 5 seconds in CLI checked console.log

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,15 +1,25 @@
-const currentPrice = require("../helpers/currentPrice");
 const { errorObject } = require('../config');
+const  { fetchCurrentPrice } = require("../helpers/currentPrice")
 
-exports.CurrentPrice = async (req, res) => {
-    try {
-        let prices = await currentPrice();
-        if (prices.error) return res.status(500).json(errorObject);
-        return res.status(200).json({
-            success: true,
-            price_data: prices.data,
-        });
-    } catch (error) {
-        return res.status(500).json(errorObject);
+// Current price controllers
+const CurrentPriceController = () => {
+    return {
+        getCurrentPrice: async (req, res) => {
+            try {
+                let prices = await fetchCurrentPrice();
+                // if (prices.error) return res.status(500).json(errorObject);
+                // return res.status(200).json({
+                //     success: true,
+                //     price_data: prices.data,
+                // });
+
+                // make this return price fetched
+            } catch (error) {
+                // return res.status(500).json(errorObject);
+
+            }
+        }
     }
-};
+}
+
+module.exports = CurrentPriceController;

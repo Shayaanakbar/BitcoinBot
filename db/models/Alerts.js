f11a0964e3ec5da490cab6d385fe97588987a859
ShayaanAkbar <shayaanakbar1106@gmail.com>
2021-05-20 23:02:10 -0400

    temp gig model

---
+++ db/models/Alerts.js
@@ -0,0 +1,14 @@
+const Sequilize = require('sequelize');
+const db = require('../db');
+
+const Alert = db.define('alert', {
+    title: {
+        type: Sequilize.STRING,
+    },
+    title: {
+        type: Sequilize.NUMBER,
+    },
+    title: {
+        type: Sequilize.NUMBER,
+    },
+})
